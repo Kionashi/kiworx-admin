195efdc9fe4ae61f1f09cc97e2e1fc691d972383
Víctor Cardozo <vcardozof@gmail.com>
2019-10-13 12:11:35 +0200

    Adds error pages layouts

---
+++ resources/views/layouts/error/scripts.blade.php
@@ -0,0 +1,13 @@
+<!-- jQuery -->
+<script src="{{ asset('gentelella/vendors/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('gentelella/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ asset('gentelella/vendors/fastclick/lib/fastclick.js') }}"></script>
+<!-- NProgress -->
+<script src="{{ asset('gentelella/vendors/nprogress/nprogress.js') }}"></script>
+
+@yield('extended-scripts')
+
+<!-- Custom Theme Scripts -->
+<script src="{{ asset('gentelella/build/js/custom.min.js') }}"></script>
